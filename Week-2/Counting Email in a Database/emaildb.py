ba94ff023088aa31bc4aa51add6b56c56926600b
PragneshRamani <pragnesh.ramani002@gmail.com>
2017-02-26 13:57:16 +0530

    Assignments of Using Databases with Python
    Assignments of Using Databases with Python

---
+++ Week-2/Counting Email in a Database/emaildb.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+conn = sqlite3.connect('emaildb.sqlite')
+cur = conn.cursor()
+
+cur.execute('''
+DROP TABLE IF EXISTS Counts''')
+
+cur.execute('''
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
+
+fname = raw_input('Enter file name: ')
+if ( len(fname) < 1 ) : fname = 'mbox.txt'
+fh = open(fname)
+for line in fh:
+    if not line.startswith('From: ') : continue
+    pieces = line.split()
+    email = pieces[1]
+    #change in logic
+    parts = email.split('@')
+    org = parts[-1]
+    print org
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org, ))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (org, count) 
+                VALUES ( ?, 1 )''', ( org, ) )
+    else : 
+        cur.execute('UPDATE Counts SET count=count+1 WHERE org = ?', 
+            (org, ))
+    # This statement commits outstanding changes to disk each 
+    # time through the loop - the program can be made faster 
+    # by moving the commit so it runs only after the loop completes
+    conn.commit()
+
+# https://www.sqlite.org/lang_select.html
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+print
+print "Counts:"
+for row in cur.execute(sqlstr) :
+    print str(row[0]), row[1]
+
+cur.close()
+
